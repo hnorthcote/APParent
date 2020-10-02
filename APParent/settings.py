4d99384f292240c5375e7c7d4ca70b59d5692d44
Henry Northcote <hnorthcote2000@hotmail.com>
2020-10-02 08:29:51 -0600

    updating for heroku

--- APParent/settings.py
+++ APParent/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'apparent.seir@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
