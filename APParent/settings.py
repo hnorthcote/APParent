f80904308f65c38ff8d74b489897580113b08bce
Henry Northcote <hnorthcote2000@hotmail.com>
2020-10-02 14:56:03 -0600

    Updating

--- APParent/settings.py
+++ APParent/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -135,6 +135,5 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-import django_heroku
 django_heroku.settings(locals())
 
