2e10c183464addf3fbd40eb9b0fcbc83e515cff6
Henry Northcote <hnorthcote2000@hotmail.com>
2020-10-01 19:23:23 -0600

    uploading for heroku

--- APParent/settings.py
+++ APParent/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'l2!fe!t$esxh4=4bl!(2d^we6m&5q&dw-8kil!w#ri+3pw-pkp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
